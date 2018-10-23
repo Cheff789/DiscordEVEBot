4e81d54f74c6236731c09762b9866f35a380d2a9
Chinchilla One <rhaynes09@yahoo.com>
2018-10-22 22:07:18 -0400

    ran file

--- Eve interfacer/eve.py
+++ Eve interfacer/eve.py
@@ -7,6 +7,6 @@ with urllib.request.urlopen("https://esi.evetech.net/latest/universe/systems/?da
 	for sys in Systems:
 		with urllib.request.urlopen("https://esi.evetech.net/latest/universe/systems/"+str(sys)+"/?datasource=tranquility&language=en-us") as url:
 			sysinfo = json.loads(url.read().decode())
-			f.write(sysinfo['name'] +":"+str(sys))
+			f.write(sysinfo['name'] +":"+str(sys)+'\n')
 			print(sysinfo['name'] +":"+str(sys))
 f.close()
